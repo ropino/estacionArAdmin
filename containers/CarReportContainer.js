17c6be53e77a3fe087cf8c9f91d72a45ca89dddf
ropino <rocioailinpino@gmail.com>
2021-04-15 21:23:26 -0300

    maquetado listo

--- containers/CarReportContainer.js
+++ containers/CarReportContainer.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import carReport from "../components/CarReport/carReport"
+
+const Stack = createStackNavigator();
 
 const carReportContainer = () => {
     return (
         <>
-            
+           <Stack.Navigator>
+        <Stack.Screen
+          name="profile"
+          component={carReport}
+          options={({ navigation }) => ({
+            title: "",
+            headerStyle: {
+              backgroundColor: "#F9B233",
+              elevation: 0,
+              shadowColor: "transparent",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerLeft: () => (
+              <Button
+                type="clear"
+                icon={
+                  <Icon
+                    name="bars"
+                    color="white"
+                    style={{ marginRight: 10 }}
+                    size={30}
+                  />
+                }
+                onPress={() => {
+                  navigation.toggleDrawer();
+                }}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>  
         </>
     );
 };
