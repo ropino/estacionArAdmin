cf69ad1e774a716c31e41bf68b1fd430ba0a6def
ropino <rocioailinpino@gmail.com>
2021-04-19 17:40:22 -0300

    supporttttttttt

--- components/Support/support.js
+++ components/Support/support.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { View, SafeAreaView } from "react-native";
 import { Card, Text, Button } from "react-native-elements";
 import { styles } from "./supportStyle";
@@ -6,13 +6,46 @@ import Icon from "react-native-vector-icons/FontAwesome5";
 import { useNavigation } from "@react-navigation/native";
 
 const support = () => {
-    return (
-        <>
-        <SafeAreaView>
-            <Text>ESTAS EN SUPPORT </Text>
-        </SafeAreaView>
-        </>
-    );
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginTop: 50,
+            marginHorizontal: 30,
+          }}
+        >
+          <Text>Enviar mensaje al soporte tecnico</Text>
+          <Text>Solicitar llamada</Text>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginVertical: 50,
+            marginHorizontal: 30,
+          }}
+        >
+          <Icon
+            name="comment-dots"
+            size={60}
+            color="black"
+            style={{ marginRight: "5%" }}
+          />
+          <Icon
+            name="phone-alt"
+            size={50}
+            color="black"
+            style={{ marginRight: "5%" }}
+          />
+        </View>
+      </SafeAreaView>
+    </>
+  );
 };
 
 export default support;
