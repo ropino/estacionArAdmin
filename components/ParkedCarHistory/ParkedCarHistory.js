74e533acefc5893e506e2b5408d2cef0d7220eab
FlorPaez-15 <florepaez15@gmail.com>
2021-04-21 13:10:04 -0300

    para pullear

--- components/ParkedCarHistory/ParkedCarHistory.js
+++ components/ParkedCarHistory/ParkedCarHistory.js
@@ -4,9 +4,39 @@ import { View, SafeAreaView, ScrollView } from "react-native";
 import { Button, Card, Text } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { useNavigation } from "@react-navigation/native";
+import firebase from "../../back/db/firebase";
 
 const parkedCarHistory = () => {
   const navigation = useNavigation();
+
+/* 
+***traer vehiculos estacionados que ya finalizaron su estacionamiento ese mismo dia
+
+TENGO QUE TRAER EL PARKINGHISTORY FILTRANDO EL DIA Y LA ZONA QUE MACHEAN CON EL DIA ACTUAL Y LA ZONA DEL ADMIN 
+
+const getParkingCarsInfoNow = (zone) => {
+    firebase.db
+      .collection("parkings")
+      .where("zone", "==", `${zone}`)
+      .onSnapshot((querySnap) => {
+        let cars = [];
+        querySnap.forEach((doc) => {
+          cars.push(doc.data());
+        });
+        return setParkedCars(cars);
+      });
+  };
+***ordenarlos del mas reciente al mas antiguo
+*/
+//  const getParkedCar = (zone, date) => {
+//    firebase.db
+//    .collection ("users")
+//    .doc()
+//    .where("zone", "==", `${zone}`)
+//   console.log("ZONA", zone);
+   
+//  }
+
 
   return (
     <ScrollView style={{ backgroundColor: "black", flex: 1 }}>
